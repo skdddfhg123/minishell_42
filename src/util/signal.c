4fa02fa7496f8f7a1528c0070e44b9d3fc9cd9bc
Dongmin Lee <dongmlee@c2r10s3.42seoul.kr>
2022-05-30 00:24:47 +0900

    \c 수정

--- src/util/signal.c
+++ src/util/signal.c
@@ -6,7 +6,7 @@
 /*   By: dongmlee <dongmlee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 19:39:52 by dongmlee          #+#    #+#             */
-/*   Updated: 2022/05/30 00:12:43 by dongmlee         ###   ########.fr       */
+/*   Updated: 2022/05/30 00:24:20 by dongmlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,6 @@ static void	handle_signal(int sig)
 		rl_replace_line("", 0);
 		rl_redisplay();
 		g_state.exit_status = 1;
-		// tcgetattr(STDIN_FILENO, &term);
-		// term.c_lflag &= (ECHOCTL);
-		// tcsetattr(STDIN_FILENO, TCSANOW, &term);
 	}
 }
 
